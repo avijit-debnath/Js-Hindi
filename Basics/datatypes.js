fd0627fe059aade937e93228aaa90c3ee02ed051
avijit-debnath <avijitdebnath269@gmail.com>
2024-04-01 09:00:51 +0530

    datatypes

--- Basics/datatypes.js
+++ Basics/datatypes.js
@@ -1,6 +1,23 @@
 "use strict"; //treat all JS code as newer version
 
-alert("3==3");
+//alert( 3+3 ) // we are using node js not browser
 
-console.log(3+3);
-console.log()
+console.log( 3+3 )
+console.log("avijit")
+
+let name = "Hitesh"
+let age = 18
+let isLogginID = false
+
+//number => 2 to the power 53
+//bigint
+//string =>""
+//Boolean => true / false
+//null => standard value
+//undefined =>
+//symbol => unique
+
+//object
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
