91055930f122762a3dcc278e6ac86236b8ffdaea
avijit-debnath <avijitdebnath269@gmail.com>
2024-04-01 09:20:22 +0530

    ConversionOperation

---
+++ Basics/ConversionOperation.js
@@ -0,0 +1,24 @@
+let score = "33abc"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+
+//"33"=> 33
+// "33abc"=> NaN(Not a number)
+//true => 1; false=>0
+
+let isLoggedIn = "hittesh"
+
+let booleanIsLoggedIn= Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+//1 => true ; 0=>false
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber)
